1204db0b4f3b82f86b1b6d4bb7358f617503cd24
Pascal Maier <paas_m@t-online.de>
2020-01-22 22:33:47 +0100

    Create UserApi

--- exzellenzkoch/src/main/kotlin/de/psekochbuch/exzellenzkoch/application/api/UserApi.kt
+++ exzellenzkoch/src/main/kotlin/de/psekochbuch/exzellenzkoch/application/api/UserApi.kt
@@ -1,4 +1,20 @@
 package de.psekochbuch.exzellenzkoch.application.api
 
+import de.psekochbuch.exzellenzkoch.application.dto.UserDto
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RequestMapping("/api/users")
 interface UserApi {
+
+    @PostMapping("")
+    fun createUser(user:UserDto)
+
+    @PutMapping("/{userId}")
+    fun updateUser(user:UserDto)
+
+    @DeleteMapping("/{userId}")
+    fun deleteUser(id:String)
 }
