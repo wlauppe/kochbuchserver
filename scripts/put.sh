e046f76205869967dda28aeab58eb468012eebd5
wolf <whlauppe@web.de>
2020-02-09 14:51:32 +0100

    scripts geupdated

--- scripts/put.sh
+++ scripts/put.sh
@@ -1,3 +1,5 @@
 echo "führe put aus, der recipe mit id 1 überschreibt"
 echo "wenns klappt sollte der Titel \"neuer Sandkuchen\" danach lauten"
-curl -X PUT -d "@recipe.json" http://193.196.38.185:8080/api/recipes/1
+echo -n "Status Code ist: "
+curl -X PUT -d "@recipe.json" http://193.196.38.185:8080/api/recipes/1 -w "%{http_code}"
+echo
