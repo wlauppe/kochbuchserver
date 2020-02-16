6415d797691f55ed2a3458702bccaad4b42895be
wolf <whlauppe@web.de>
2020-02-16 15:33:24 +0100

    max upload increased

--- scripts/put.sh
+++ scripts/put.sh
@@ -1,5 +1,5 @@
 echo "führe put aus, der recipe mit id 1 überschreibt"
 echo "wenns klappt sollte der Titel \"neuer Sandkuchen\" danach lauten"
 echo -n "Status Code ist: "
-curl -X PUT -d "@recipe.json" http://193.196.38.185:8080/api/recipes/1 -w "%{http_code}"
+curl -X PUT -d "@recipe.json" http://193.196.38.185:8080/api/recipes/1 -w "%{http_code}" -H "Content-Type: application/json"
 echo
