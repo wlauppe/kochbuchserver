e48816e8949236e9ae163c3f5c8fd86fe47757fe
PassM <uhkcw@student.kit.edu>
2020-02-22 02:55:28 +0100

    Add Favourite API and controller

---
+++ exzellenzkoch/src/main/kotlin/de/psekochbuch/exzellenzkoch/application/api/FavouritesApi.kt
@@ -0,0 +1,42 @@
+package de.psekochbuch.exzellenzkoch.application.api
+
+import de.psekochbuch.exzellenzkoch.application.dto.PublicRecipeDto
+import org.springframework.web.bind.annotation.*
+
+/**
+ * Interface for the api to handle favourites
+ */
+@RequestMapping("api/users")
+interface FavouritesApi
+{
+    /**
+     * POST-Request to add a favourite to an user.
+     * The URL ends with /api/users/userId/favourites/recipeId
+     * @param userId The id of the user
+     * @param recipeId The id of the recipe
+     */
+    @PutMapping("/{userId}/favourites/{recipeId}")
+    fun addFavRecipe(@PathVariable userId:String, @PathVariable recipeId:Int)
+
+    /**
+     * GET-Request to get the favourites of an user.
+     * The URl ends with /api/users/userId/favourites
+     * @param userId Id of the user
+     * @param page The page of the already loaded recipes
+     * @param readCount Count of the to loaded recipes
+     * @return The favourite recipes from an user
+     */
+    @GetMapping("/{userId}/favourites")
+    fun getFavRecipes(@PathVariable userId: String,
+                      @RequestParam("page") page:Int,
+                      @RequestParam("readCount") readCount:Int) :List<PublicRecipeDto>?
+
+    /**
+     * DELETE-Request to delete a favourite of an user
+     * The URL ends with /api/users/userId/favourites/recipeId
+     * @param userId The id of the user
+     * @param recipeId The id of the recipe
+     */
+    @DeleteMapping("/{userId}/favourites/{recipeId}")
+    fun delFavRecipe(@PathVariable userId: String, @PathVariable recipeId:Int)
+}
---
+++ exzellenzkoch/src/main/kotlin/de/psekochbuch/exzellenzkoch/application/controller/FavouritesController.kt
@@ -0,0 +1,24 @@
+package de.psekochbuch.exzellenzkoch.application.controller
+
+import de.psekochbuch.exzellenzkoch.application.api.FavouritesApi
+import de.psekochbuch.exzellenzkoch.application.dto.PublicRecipeDto
+import de.psekochbuch.exzellenzkoch.application.service.FavouritesService
+import org.springframework.beans.factory.annotation.Autowired
+
+class FavouritesController : FavouritesApi {
+
+    @Autowired
+    private val service : FavouritesService? = null
+
+    override fun addFavRecipe(userId: String, recipeId: Int) {
+        service?.addFavRecipe(userId,recipeId)
+    }
+
+    override fun getFavRecipes(userId: String, page: Int, readCount: Int): List<PublicRecipeDto>? {
+        return service?.getFavRecipes(userId,page,readCount)
+    }
+
+    override fun delFavRecipe(userId: String, recipeId: Int) {
+        service?.delFavRecipe(userId,recipeId)
+    }
+}
