281eb160f995282a44df503baa65812f35b41ddf
wolf <whlauppe@web.de>
2020-02-28 12:21:21 +0100

    added environment variables

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export DATASOURCE_URL=$(hostname -I | cut -d' ' -f1)
-export DATASOURCE_USERNAME=test
-export DATASOURCE_USERNAME=test
+export DB_IP=$(hostname -I | cut -d' ' -f1)
+export DATASOURCE_NAME=pseDb
+export DATASOURCE_USERNAME=pseUser
+export DATASOURCE_USERPASSWORD=n2pIuTff
 
 java -jar exzellenzkoch-0.0.1-SNAPSHOT.jar
