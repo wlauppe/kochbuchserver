120d3ee3be0b28884b866af03493330f49509e9d
wolf <whlauppe@web.de>
2020-03-05 10:39:27 +0100

    start server sh geupdated

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@ export DATASOURCE_NAME=pseDb
 export DATASOURCE_USERNAME=pseUser
 export DATASOURCE_USERPASSWORD=n2pIuTff
 
-java -jar exzellenzkoch-0.0.1-SNAPSHOT.jar
+java -jar exzellenzkoch-0.0.1-SNAPSHOT.jar | tee server.log
