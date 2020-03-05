3e4e30f90640742d27f003d1708ba9bd2d780b78
wolf <whlauppe@web.de>
2020-03-05 12:32:57 +0100

    updated cleanup

--- scripts/cleanup_test.sh
+++ scripts/cleanup_test.sh
@@ -1,3 +1,5 @@
 echo "bilder werden gelöscht und Datenbank auf den initialzustand gesetzt"
+export DATASOURCE_NAME=pseDb
+sudo mysql -u root -e "DROP DATABASE $DATASOURCE_NAME"
 sudo mysql -u root <~/kochbuchserver/Db.sql
 rm -rf ~/images/*
