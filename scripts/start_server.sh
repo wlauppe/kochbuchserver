19add850cd7dadd9bac46daafefc019827e7ea51
wolf <whlauppe@web.de>
2020-03-06 11:42:29 +0100

    fix scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export DB_IP=$(hostname -I | cut -d' ' -f1)
+#export DB_IP=$(hostname -I | cut -d' ' -f1)
+export DB_IP=localhost
 export DATASOURCE_NAME=pseDb
 export DATASOURCE_USERNAME=pseUser
 export DATASOURCE_USERPASSWORD=n2pIuTff
